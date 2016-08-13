1216518240e07bc68137e3e7fa7bfe3b8fb1a65b
nobuGT <nobuyuki2002t@gmail.com>
2016-08-13 22:10:15 +0900

    Update SetNewmob.js

--- SetNewmob.js
+++ SetNewmob.js
@@ -1,3 +1,7 @@
-//2016:8/13/22:03
-function SetNewmob(name,monsterID,RenderType,Skin,HitPoint){
+//2016:8/13/22:10
+function SetNewmob(name,mobID,RenderType,Skin,HitPoint){
+name=Level.spawnMob(x,y+1,z,mobID,"mob/Skin");
+Entity.setRenderType(name,RenderType);
+Entity.setHealth(name,HitPoint);
+Entity.setNameTag(name,"name");
 }
