ba3118768b8ed7d37d6dcbcb0f0b8ec5d7f7ff9c
nobuGT <nobuyuki2002t@gmail.com>
2016-08-15 12:16:55 +0900

    Update NewMobTypeFunction.js

--- NewMobTypeFunction.js
+++ NewMobTypeFunction.js
@@ -1,9 +1,12 @@
 //2016:8/13/22:
 function newMobType(entity,mobTypeID){
-mobTypeID:TypeDifence{
+mobTypeID:TypeNomale{
 Ex=Entity.getX(entity);
 Ey=Entity.getY(entity);
 Ez=Entity.getZ(entity);
 Px=Player.getX();
 Py=player.getY();
 Pz=player.getZ();
+if(Px+3>Ex&&Px-3>Ex&&Py-3||0>Ey&&Pz+3<Ez&&Pz-3<Ez){
+setVelX(entity,(Px+3||Px-3<Ex));
+setVelZ(entity,(Pz+3||Pz-3<Ez));
